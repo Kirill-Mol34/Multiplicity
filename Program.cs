2a750383e55be92e7eb8ea1357f77e49df580d5b
Kirill <kir.molokoff@yandex.ru>
2023-01-20 19:39:49 +0900

    fin

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Введите первое число: ");
+int Number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int Number2 = Convert.ToInt32(Console.ReadLine());
+int result = 0;
+int MultiplicityNumber()
+{
+    result = Number1 % Number2;
+    return result;
+}
+MultiplicityNumber();
+System.Console.WriteLine();
+if (result == 0) System.Console.WriteLine($"Число {Number1} кратно числу {Number2}");
+else
+{
+    Console.WriteLine($"Число {Number1} не кратно числу {Number2}, остаток от деления равен {result}");
+}
+System.Console.WriteLine();
